ef6ef3cfb13d9493f1210acb0b59849b1b68e91d
Kilian-Sosa <kilianjosesosaguillen@alumno.ieselrincon.es>
2023-10-12 23:01:44 +0100

    feat: Add audio playback control

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
 export const Pause = ({ className }) => (
     <svg className={className} role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg>
@@ -18,6 +18,19 @@ export const Volume = () => (
 
 export function Player () {
     const [isPlaying, setIsPlaying] = useState(false)
+    const [currentSong, setCurrentSong] = useState(null)
+    const audioRef = useRef()
+
+    const handleClick = () => {
+        if (isPlaying) {
+            audioRef.current.pause()
+        } else {
+            audioRef.current.src = `/music/1/03.mp3`
+            audioRef.current.play()
+            audioRef.current.volume = 0.1
+        }
+        setIsPlaying(!isPlaying)
+    }
 
     return (
         <div className="flex flex-row justify-betweent w-full px-4 z-50">
@@ -27,15 +40,17 @@ export function Player () {
 
             <div className="grid place-content-center gap-4 flex-1">
                 <div className="flex justify-center">
-                    <button className="bg-white rounded-full p-2" onClick={() => setIsPlaying(!isPlaying)}>
+                    <button className="bg-white rounded-full p-2" onClick={handleClick}>
                         {isPlaying ? <Pause /> : <Play />}
                     </button>
                 </div>
             </div>
 
-            <div className="">
+            <div className="grid place-content-center">
                 Volume...
             </div>
+
+            <audio ref={audioRef} />
         </div>
     )
 }
