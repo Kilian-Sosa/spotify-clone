ba8f28a249fcab93701bbe663c7213749c5d0777
Kilian-Sosa <kilianjosesosaguillen@alumno.ieselrincon.es>
2023-10-13 22:07:08 +0100

    fix: Maintain initial volume

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -40,6 +40,7 @@ const CurrentSong = ({ image, title, artists }) => {
 export function Player () {
     const { currentMusic, isPlaying, setIsPlaying } = usePlayerStore(state => state)
     const audioRef = useRef()
+    const volumeRef = useRef(1)
 
     useEffect(() => {
         isPlaying
@@ -52,6 +53,7 @@ export function Player () {
         if (song) {
             const src = `/music/${playlist?.id}/0${song.id}.mp3`
             audioRef.current.src = src
+            audioRef.current.volume = volumeRef.current
             audioRef.current.play()
         }
     }, [currentMusic])
@@ -82,7 +84,9 @@ export function Player () {
                     className="w-[95px]"
                     onValueChange={(value) => {
                         const [newVolume] = value
-                        audioRef.current.volume = newVolume / 100
+                        const volumeValue = newVolume / 100
+                        volumeRef.current = volumeValue
+                        audioRef.current.volume = volumeValue
                     }
                 } />
             </div>
